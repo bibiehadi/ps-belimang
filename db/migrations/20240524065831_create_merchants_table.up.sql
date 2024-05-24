3b6670ec0b87fb53ac209e3c69434b97fb698ee6
husni mubarok <husni060300@gmail.com>
2024-05-25 03:17:36 +0700

    feat : merchant entity

--- db/migrations/20240524065831_create_merchants_table.up.sql
+++ db/migrations/20240524065831_create_merchants_table.up.sql
@@ -3,7 +3,8 @@ CREATE TABLE merchants (
     name VARCHAR(255) NOT NULL,
     merchant_category VARCHAR(100) NOT NULL,
     image_url TEXT,
-    latitude VARCHAR(50),
-    longitude VARCHAR(50),
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
